84fa46412920eef29ff83612b240227b6ac2f5b4
Neha Agrawal <neha260492@gmail.com>
2018-01-08 13:52:23 +0530

    PrePopulate slug field in Category Admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+# Class to customize admin interface
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = { 'slug':('name',)}
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
